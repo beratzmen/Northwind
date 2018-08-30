c9a9f6e7f9c320489252d9afa4fc5e57754a2934
bzmen <beratzmen@gmail.com>
2018-08-30 09:53:54 +0300

    30_08

---
+++ Northwind.MvcUI/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Northwind.Entities;
+using Northwind.Interfaces;
+using Northwind.MvcUI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Northwind.MvcUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public int PageSize = 5;
+
+        public ViewResult Index(int page = 1)
+        {
+            List<Products> products = _productService.GetAll();
+            //View'a ürünlerin detaylarını gönderiyorum , listelenecek sayfa sayısı,1 sayfadaki ürün sayısı ..
+            return View(new ProductViewModel
+            {
+                Products = products.Skip((page - 1) * PageSize).Take(5).ToList(),
+
+                PagingInfo = new PagingInfo
+                {
+                    ItemsPerPage = PageSize,
+                    TotalItems = products.Count,
+                    CurrentPage = page
+                }
+            });
+        }
+    }
+}
---
+++ Northwind.MvcUI/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Northwind.Entities;
+
+namespace Northwind.MvcUI.Models
+{
+    public class ProductViewModel
+    {
+        public PagingInfo PagingInfo { get; internal set; }
+        public List<Products> Products { get; set; }
+    }
+}
